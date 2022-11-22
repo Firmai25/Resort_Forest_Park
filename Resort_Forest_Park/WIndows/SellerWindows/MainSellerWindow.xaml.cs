f46ca1fce0107687506915cf9723aaa6ce610d0b
unknown <den.cherepanov.03@mail.ru>
2022-11-22 20:11:57 +0500

    Add Main Seller Window

---
+++ Resort_Forest_Park/WIndows/SellerWindows/MainSellerWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Resort_Forest_Park.WIndows.SellerWindows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainSellerWindow.xaml
+    /// </summary>
+    public partial class MainSellerWindow : Window
+    {
+        public MainSellerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
