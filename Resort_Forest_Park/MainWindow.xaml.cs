5a79e41dc18ed7e68863e9459f471acc5688f161
unknown <den.cherepanov.03@mail.ru>
2022-11-28 16:07:06 +0500

    Add captcha window

--- Resort_Forest_Park/MainWindow.xaml.cs
+++ Resort_Forest_Park/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Resort_Forest_Park.Entities;
+using Resort_Forest_Park.WIndows;
 using Resort_Forest_Park.WIndows.SellerWindows;
 using Resort_Forest_Park.WIndows.ShiftSupervisorWindows;
 using Resort_Forest_Park.WIndows.AdministratorWindows;
@@ -28,15 +29,22 @@ namespace Resort_Forest_Park
         {
             InitializeComponent();
         }
-
+        int CountError = 0;
         private void Login_Click(object sender, RoutedEventArgs e)
         {
             Forest_ParkEntities db = new Forest_ParkEntities();
             Worker worker = db.Workers.Where(b => b.Login == TbName.Text && b.Password == PbPassword.Password).FirstOrDefault();
             if (worker != null)
             {
+                LoginHistory loginHistory = new LoginHistory();
+                    loginHistory.ID_Worker = worker.ID;
+                    loginHistory.Type_Login = "Успешно";
+                    loginHistory.Time_Login = DateTime.Now.ToString();
+                    db.LoginHistories.Add(loginHistory);
+                    db.SaveChanges();
                 switch(worker.TypeWorker.Name)
                 {
+
                     case "Администратор":
                         MainAdminWindow mainAdminWindow = new MainAdminWindow();
                         mainAdminWindow.Show();
@@ -56,7 +64,28 @@ namespace Resort_Forest_Park
             }
             else
             {
+                worker = db.Workers.Where(b => b.Login == TbName.Text).FirstOrDefault();
+                if (worker != null)
+                {
+                    LoginHistory loginHistory = new LoginHistory();
+                    loginHistory.ID_Worker = worker.ID;
+                    loginHistory.Type_Login = "Неуспешно";
+                    loginHistory.Time_Login = DateTime.Now.ToString();
+                    db.LoginHistories.Add(loginHistory);
+                    db.SaveChanges();
+                }
                 MessageBox.Show("Вы ввели неправильные данные");
+                CountError++;
+                if(CountError> 2) 
+                {
+                    CaptchaWindow captchaWindow = new CaptchaWindow();
+                    captchaWindow.Owner= this;
+                    captchaWindow.ShowDialog();
+                    if (captchaWindow.Block == true)
+                    {
+                        MainGrid.IsEnabled = false;
+                    }
+                }
             }
         }
     }
---
+++ Resort_Forest_Park/WIndows/CaptchaWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Shapes;
+using Brush = System.Drawing.Brush;
+using Brushes = System.Drawing.Brushes;
+using Color = System.Windows.Media.Color;
+using Point = System.Drawing.Point;
+
+namespace Resort_Forest_Park.WIndows
+{
+    /// <summary>
+    /// Логика взаимодействия для CaptchaWindow.xaml
+    /// </summary>
+    public partial class CaptchaWindow : Window
+    {
+        public CaptchaWindow()
+        {
+            InitializeComponent();
+            IgCapt.Source = ImageSourceFromBitmap(CreateImage(200, 50));
+        }
+
+        private string text = String.Empty;
+        private Bitmap CreateImage(int Width, int Height)
+        {
+            Random rnd = new Random();
+
+            //Создадим изображение
+            Bitmap result = new Bitmap(Width, Height);
+
+            //Вычислим позицию текста
+            int Xpos = rnd.Next(0, Width - 95);
+            int Ypos = rnd.Next(15, Height - 25);
+
+            //Добавим различные цвета
+            Brush[] colors = { Brushes.Black,
+                     Brushes.Red,
+                     Brushes.Green,
+                     Brushes.DarkBlue};
+
+            //Укажем где рисовать
+            Graphics g = Graphics.FromImage((System.Drawing.Image)result);
+
+            //Пусть фон картинки будет серым
+            g.Clear(System.Drawing.Color.Gray);
+
+            //Сгенерируем текст
+            text = String.Empty;
+            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+            for (int i = 0; i < 5; ++i)
+                text += ALF[rnd.Next(ALF.Length)];
+
+            //Сгенерируем формат текста
+            string[] st =
+            {
+                "Arial",
+                "Times New Roman",
+                "Lucida Calligraphy"
+            };
+            //Нарисуем сгенирируемый текст
+            g.DrawString(text,
+                         new Font(st[rnd.Next(st.Length)], 15),
+                         colors[rnd.Next(colors.Length)],
+                         new PointF(Xpos, Ypos));
+
+            //Добавим немного помех
+            /////Линии из углов
+            g.DrawLine(Pens.Black,
+                       new Point(0, 0),
+                       new Point(Width - 1, Height - 1));
+            g.DrawLine(Pens.Black,
+                       new Point(0, Height - 1),
+                       new Point(Width - 1, 0));
+            ////Белые точки
+            for (int i = 0; i < Width; ++i)
+                for (int j = 0; j < Height; ++j)
+                    if (rnd.Next() % 20 == 0)
+                        result.SetPixel(i, j, System.Drawing.Color.White);
+
+            return result;
+        }
+
+        //If you get 'dllimport unknown'-, then add 'using System.Runtime.InteropServices;'
+        [System.Runtime.InteropServices.DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        public ImageSource ImageSourceFromBitmap(Bitmap bmp)
+        {
+            var handle = bmp.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+
+        private void UpdateCapt_Click(object sender, RoutedEventArgs e)
+        {
+            IgCapt.Source = ImageSourceFromBitmap(CreateImage(200, 50));
+        }
+        public bool Block = true;
+        private void CheckCapt_Click(object sender, RoutedEventArgs e)
+        {
+            if(TbTextCapt.Text == text)
+            {
+                Block = false;
+            }
+            else
+            {
+                Block = true;
+            }
+            Close();
+        }
+    }
+}
